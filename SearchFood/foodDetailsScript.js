8c3e696a7d7b748e2f09b039b040342451a70df6
webdev-chinmoy-biswas <chinmoy709@gmail.com>
2022-08-30 21:18:13 +0600

    first commit

---
+++ SearchFood/foodDetailsScript.js
@@ -0,0 +1,8 @@
+function showDetails (id) {
+    // const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(data => console.log(data.meals[0]))
+    console.log('show Details', id)
+}
+
