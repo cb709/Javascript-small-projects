4b158293f3da290e706263f77c8776af94a814a3
webdev-chinmoy-biswas <chinmoy709@gmail.com>
2022-09-02 13:34:01 +0600

    added phone hunter

--- phone-hunter/script.js
+++ phone-hunter/script.js
@@ -1,9 +1,110 @@
- async function loadData(text) {
-    const url = `https://openapi.programming-hero.com/api/phones?search=${text}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data) ;
+const mainHeading = document.getElementById("main-heading");
+const phonesContainer = document.getElementById("phones-container");
 
+//========== search form input text ==========
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchText = document.getElementById("search-input").value;
+  if (searchText === "") {
+    alert("Enter phone name to search");
+  } else {
+    loadPhonesData(searchText);
+  }
+});
+
+//=============== fetching data with a keyword
+
+const loadPhonesData = async (text) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${text}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  showPhones(data);
+};
+
+//======= showing phones based on api data
+
+const showPhones = (data) => {
+  const phones = data.data;
+  if (phones.length == 0) {
+    phonesContainer.innerHTML = ` `;
+    mainHeading.innerHTML = `No Phones Found !`;
+  } else {
+    phonesContainer.innerHTML = ` `;
+    mainHeading.innerHTML = `Phone Hunter`;
+    phones.forEach((phone) => {
+      phonesContainer.innerHTML += `
+        <div class="col-lg-3 col-sm-6 col-6 mb-3">    
+
+                <div class="card h-100">
+                <a class="btn p-3" data-toggle="modal" data-target="#phone-details" onclick="getPhoneDetails('${phone.slug}')">
+                    <img src="${phone.image}" class="card-img-top"/>
+                    <div class="card-body">
+                        <h6 class="card-title">${phone.brand} - ${phone.phone_name}</h6>
+                    </div>
+                    </a>   
+                </div>
+ 
+        </div>    
+    `;
+    });
+  }
+};
+
+// =======Get phone deatils from api==========
+const getPhoneDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showPhoneDetails(data));
 }
 
-loadData('all')
+// ==================show phone details in modal==========
+const showPhoneDetails = (data) => {
+    console.log(data.data)
+    const {
+        brand,
+        name,
+        releaseDate,
+        mainFeatures: {
+            chipSet,
+            displaySize,
+            memory,
+            sensors,
+            storage
+        }
+    } = data.data;
+
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = `
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="phone-detailsLabel">${brand} - ${name}</h5>
+                <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                >
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <p><b>Release Date:</b> ${releaseDate}</p>
+            <p><b>Chipset:</b> ${chipSet}</p>
+            <p><b>Memory:</b> ${memory}</p>
+            <p><b>Display:</b> ${displaySize}</p>
+            <p><b>Storage:</b> ${storage}</p>
+            <p><b>Sensors:</b> ${[...sensors]}</p>
+            </div>
+            <div class="modal-footer">
+                <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+                >
+                    Close
+                </button>
+            </div>
+        </div>
+    `
+}
+loadPhonesData("iphone");
--- script.js
+++ script.js
@@ -6,11 +6,17 @@ const projects = [
     link: "../search-food/index.html"
   },
   {
+    name: "Phone Hunter",
+    tools: "HTML, CSS, Bootstrap, JS, Programming Hero API",
+    description: "Simple Phone search app with Programming hero Phone Hunter api",
+    link: "../phone-hunter/index.html"
+  },
+  {
     name: "Country info App",
     tools: "HTML, CSS, Bootstrap, JS, Rest Countries API",
     description: "Simple Country search app with Rest Countries api",
     link: "../countries-info/index.html"
-  },
+  }
 ];
 
 
@@ -21,15 +27,15 @@ function showProjects(projects) {
   totalProjects.forEach((project) => {
     projectContainer.innerHTML += `
         <div class="card-deck col-lg-4 col-md-6 mb-4 project-card">
-            <div class="card">
+          <div class="card bg-dark border text-white">
             <div class="card-body">
                 <h5 class="card-title">${project.name}</h5>
                 <p class="card-text">Tools: ${project.tools}</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><a href="${project.link}" >View Project</a></small>
+                <small class="text-white"><a class="text-white" href="${project.link}" >View Project</a></small>
             </div>
-            </div>
+          </div>
         </div>
         `;
   });
